006b69a3f371f67d9384109938b4a5449d1c6235
linchengdong <linchengdong@ins24.com>
2019-12-04 19:05:01 +0800

    add commander parse

---
+++ src/constsants.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 常量文件
+ * @Author: chengDong
+ * @Date: 2019-12-04 17:43:16
+ * @LastEditTime: 2019-12-04 18:01:15
+ * @LastEditors: chengDong
+ */
+const {version} = require('../package.json')
+
+module.exports = {
+    version
+}
--- src/main.js
+++ src/main.js
@@ -2,8 +2,61 @@
  * @Description: 业务逻辑核心文件
  * @Author: chengDong
  * @Date: 2019-12-04 17:23:44
- * @LastEditTime: 2019-12-04 17:33:31
+ * @LastEditTime: 2019-12-04 19:04:31
  * @LastEditors: chengDong
  */
 
-console.log(111)
+const commander = require('commander');
+const {version} = require('./constsants')
+// un-cli create xxx
+
+const mapAction ={
+    create:{
+        alias:'c',
+        description:'create a project',
+        examples:[
+            'un-cli create <projectName>'
+        ]
+    },
+    config: {
+        alias:'conf',
+        description:'configure a project',
+        examples:[
+            'un-cli config set <key> <value>',
+            'un-cli config get <key>'
+        ]
+    },
+    '*': {
+        alias: '',
+        description: 'command not find',
+        "examples":[
+            
+        ]
+    }
+}
+
+
+Reflect.ownKeys(mapAction).forEach((action) => { 
+    commander.command(action)// 配置命令的名字
+    .alias(mapAction[action].alias) // 命令的别名
+    .description(mapAction[action].description) // 命令的描述
+    .action(() => {
+        if(action === '*') { 
+            console.log(mapAction[action].description)
+        }
+    })
+})
+
+
+/**
+ * 监听用户的help操作
+ */
+commander.on('--help',() => {
+    Reflect.ownKeys(mapAction).forEach((action) => {
+        mapAction[action].examples.forEach(example => {
+            console.log(` ${example}`)
+        })
+    })
+})
+// 解析用户传递的参数
+commander.version(version).parse(process.argv)
