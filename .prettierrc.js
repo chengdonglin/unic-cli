20302c5b37ec8c7b025f7f0ec5a0dd54f998b707
linchengdong <linchengdong@ins24.com>
2019-12-05 09:54:55 +0800

    add pretty rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,12 +2,14 @@
  * @Description: code rules
  * @Author: chengDong
  * @Date: 2019-12-05 09:41:13
- * @LastEditTime: 2019-12-05 09:41:53
+ * @LastEditTime: 2019-12-05 09:53:18
  * @LastEditors: chengDong
  */
 module.exports = {
     trailingComma: 'es5',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
+    tabWidth: 4, // 缩进字节数
+    semi: false, // 句尾加分号
+    singleQuote: true, // 使用单引号代替双引号
+    bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }""
+    arrowParens: 'avoid', //(x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
 }
--- src/create.js
+++ src/create.js
@@ -1,27 +1,30 @@
 /*
  * @Author: chengDong
  * @Date: 2019-12-04 21:13:34
- * @LastEditTime: 2019-12-05 09:29:29
+ * @LastEditTime: 2019-12-05 09:54:24
  * @LastEditors: chengDong
  * @Description: create project code
  * @FilePath: \node-dev-template\src\create.js
  */
 
 /**
-  * 拉取自己所有的项目, 选择完之后, 选择版本
-  * https://api.github.com/users/chengdonglin/repos
-  */
+ * 拉取自己所有的项目, 选择完之后, 选择版本
+ * https://api.github.com/users/chengdonglin/repos
+ */
 
-const axios = require('axios');
+const axios = require('axios')
 
 const fetchRepoList = async () => {
-  const { data } = await axios.default.get('https://api.github.com/users/chengdonglin/repos');
-  return data;
-};
+    const { data } = await axios.default.get(
+        'https://api.github.com/users/chengdonglin/repos'
+    )
+    return data
+}
 
-module.exports = async (projectName) => {
-  console.log(projectName);
-  let repos = await fetchRepoList();
-  repos = repos.map((item) => item.name);
-  console.log(repos);
-};
+module.exports = async projectName => {
+    console.log(projectName)
+    let repos = await fetchRepoList()
+    repos = repos.map(item => item.name)
+    console.log(repos)
+    console.log('xxx')
+}
