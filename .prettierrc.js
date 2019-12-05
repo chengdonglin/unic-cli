754250c9cde7d8d86c92d9fae82bdc2dd67354a5
linchengdong <linchengdong@ins24.com>
2019-12-05 09:45:03 +0800

    add pretty-quick

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: code rules
+ * @Author: chengDong
+ * @Date: 2019-12-05 09:41:13
+ * @LastEditTime: 2019-12-05 09:41:53
+ * @LastEditors: chengDong
+ */
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+}
