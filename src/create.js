7ad12c6725638b7ccdbf7b6384b42609a6986fcc
chengdonglin <chengdong2518@163.com>
2019-12-08 19:57:08 +0800

    choise the template

--- src/create.js
+++ src/create.js
@@ -1,8 +1,8 @@
 /*
  * @Author: chengDong
  * @Date: 2019-12-04 21:13:34
- * @LastEditTime: 2019-12-05 09:57:27
- * @LastEditors: chengDong
+ * @LastEditTime: 2019-12-08 19:55:43
+ * @LastEditors: Please set LastEditors
  * @Description: create project code
  * @FilePath: \node-dev-template\src\create.js
  */
@@ -13,18 +13,31 @@
  */
 
 const axios = require('axios');
+const ora = require('ora');
+const Inquirer = require('inquirer');
 
+/**
+ * 获取远程模板
+ */
 const fetchRepoList = async () => {
   const { data } = await axios.default.get(
-    'https://api.github.com/users/chengdonglin/repos'
+    'https://api.github.com/orgs/unic-lin/repos'
   );
   return data;
 };
 
 module.exports = async (projectName) => {
-  console.log(projectName);
+    const spinner = ora('fetching template ....');
+    spinner.start();
   let repos = await fetchRepoList();
+  spinner.succeed();
   repos = repos.map((item) => item.name);
-  console.log(repos);
-  console.log('xxx');
+  // 选择模板
+  const { repo } = await Inquirer.prompt({
+      name: 'repo', //获取选择的结果
+      type:'list',
+      choices:repos,
+      message:'please choise a node template to create koa  web server to quick start development'
+  })
+  console.log(repo)
 };
